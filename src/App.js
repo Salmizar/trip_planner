e7ed799a53d57efdf0080ecd8057361d22b7708e
Salmizar <chris@salmizar.com>
2023-04-20 14:30:31 -0400

    Add files and folders added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
-
-const App = () => {
-    
+import './app.css';
+import { BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from './components/login/login';
+import Dashboard from './components/dashboard/dashboard';
+import Register from './components/register/register';
+import Reset from './components/reset/reset';
+const App = () => {    
     return (
         <div className="App">
-            Trip Planner
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/register/" element={<Register />} />
+                    <Route path="/dashboard/" element={<Dashboard />} />
+                    <Route path="/reset/" element={<Reset />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
