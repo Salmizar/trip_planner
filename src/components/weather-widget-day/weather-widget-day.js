611aadf3c65bab05465f106c2f4c3df43a43ff2c
Salmizar <chris@salmizar.com>
2023-05-01 11:08:00 -0400

    Code Clean up and css optimizations

--- src/components/weather-widget-day/weather-widget-day.js
+++ src/components/weather-widget-day/weather-widget-day.js
@@ -8,17 +8,17 @@ const WeatherWidgetDay = ({ forecastInfo, weatherIconDescription, firstEntry }) 
             <div className={'weather-widget-forecast-date ' + (firstEntry ? 'block' : 'hidden') + ' text-gray-500'}>
                 {Utils.WeatherUtils.daysOfWeek[forecastData.forecast_day_of_week] + ' ' + forecastData.forecast_day}
             </div>
-            <div className='weather-widget-night-container'>
-                <div className='weather-widget-night-container-title'>
+            <div className='weather-widget-forecast-container'>
+                <div className='weather-widget-forecast-title'>
                     {forecastData.night_temp + '°C'}
                 </div>
-                <div className={'weather-widget-night-container-feels ' + ((forecastData.windchill_min === forecastData.night_temp) ? 'invisible' : '')}>
+                <div className={'weather-widget-forecast-feels ' + ((forecastData.windchill_min === forecastData.night_temp) ? 'invisible' : '')}>
                     Feels like {forecastData.windchill_min}°C
                 </div>
-                <img className='weather-widget-night-container-weather-ico' src={'/assets/weatherIcons/' + forecastData.night_weather_icon.substring(3) + 'n.png'}></img>
-                <div className='weather-widget-container-description'>
-                    {weatherIconDescription[forecastData.night_weather_icon.substring(3)]}
-                    <div className='weather-widget-container-wind-direction'>
+                <img className='weather-widget-forecast-weather-ico' src={'/assets/weatherIcons/' + forecastData.night_weather_icon.substring(3) + 'n.png'}></img>
+                <div className='weather-widget-forecast-description'>
+                    <span>{weatherIconDescription[forecastData.night_weather_icon.substring(3)]}</span>
+                    <div className='weather-widget-forecast-wind-direction'>
                         <svg height="10" width="8" style={{ transform: ' rotate(' + forecastData.night_wind_direction + 'deg)' }}>
                             <path d="M4 0 L0 10 L4 6 L8 10 Z" />
                         </svg>
@@ -26,23 +26,23 @@ const WeatherWidgetDay = ({ forecastInfo, weatherIconDescription, firstEntry }) 
                             {Utils.WeatherUtils.getWindDirection(forecastData.night_wind_direction)}</div>
                     </div>
                 </div>
-                <div className='weather-widget-container-precipitation'>
-                    <span>{forecastData.night_rain}&nbsp;{forecastData.night_snow}</span>
-                    <div className='weather-widget-container-precipitation-rain' style={{ height: (parseFloat(forecastData.night_rain) * 2) + 'px', display: ((parseFloat(forecastData.night_rain) > 0) ? 'block' : 'none') }}></div>
-                    <div className='weather-widget-container-precipitation-snow' style={{ height: (parseFloat(forecastData.night_snow) * 2) + 'px', display: ((parseFloat(forecastData.night_snow) > 0) ? 'block' : 'none') }}></div>
+                <div className='weather-widget-forecast-precipitation'>
+                    <span>{forecastData.night_rain}<br style={{ display: ((parseFloat(forecastData.night_snow) > 0 && parseFloat(forecastData.night_snow) < 18) ? 'block' : 'none') }}></br>{forecastData.night_snow}</span>
+                    <div className='weather-widget-forecast-precipitation-rain' style={{ height: (parseFloat(forecastData.night_rain) * 2) + 'px', display: ((parseFloat(forecastData.night_rain) > 0) ? 'block' : 'none') }}></div>
+                    <div className='weather-widget-forecast-precipitation-snow' style={{ height: (parseFloat(forecastData.night_snow) * 2) + 'px', display: ((parseFloat(forecastData.night_snow) > 0) ? 'block' : 'none') }}></div>
                 </div>
             </div>
-            <div className='weather-widget-day-container'>
-                <div className='weather-widget-day-container-title'>
+            <div className='weather-widget-forecast-container'>
+                <div className='weather-widget-forecast-title'>
                     {forecastData.day_temp + '°C'}
                 </div>
-                <div className={'weather-widget-day-container-feels ' + ((forecastData.windchill_max === forecastData.day_temp) ? 'invisible' : '')}>
+                <div className={'weather-widget-forecast-feels ' + ((forecastData.windchill_max === forecastData.day_temp) ? 'invisible' : '')}>
                     Feels like {forecastData.windchill_max}°C
                 </div>
-                <img className='weather-widget-day-container-weather-ico' src={'/assets/weatherIcons/' + forecastData.day_weather_icon.substring(3) + 'd.png'}></img>
-                <div className='weather-widget-container-description'>
-                    {weatherIconDescription[forecastData.day_weather_icon.substring(3)]}<br></br>
-                    <div className='weather-widget-container-wind-direction'>
+                <img className='weather-widget-forecast-weather-ico' src={'/assets/weatherIcons/' + forecastData.day_weather_icon.substring(3) + 'd.png'}></img>
+                <div className='weather-widget-forecast-description'>
+                    <span>{weatherIconDescription[forecastData.day_weather_icon.substring(3)]}</span>
+                    <div className='weather-widget-forecast-wind-direction'>
                         <svg height="10" width="8" style={{ transform: ' rotate(' + forecastData.day_wind_direction + 'deg)' }}>
                             <path d="M4 0 L0 10 L4 6 L8 10 Z" />
                         </svg>
@@ -50,10 +50,10 @@ const WeatherWidgetDay = ({ forecastInfo, weatherIconDescription, firstEntry }) 
                             {Utils.WeatherUtils.getWindDirection(forecastData.day_wind_direction)}</div>
                     </div>
                 </div>
-                <div className='weather-widget-container-precipitation'>
-                    <span>{forecastData.day_rain}&nbsp;{forecastData.day_snow}</span>
-                    <div className='weather-widget-container-precipitation-rain' style={{ height: (parseFloat(forecastData.day_rain) * 2) + 'px', display: ((parseFloat(forecastData.day_rain) > 0) ? 'block' : 'none') }}></div>
-                    <div className='weather-widget-container-precipitation-snow' style={{ height: (parseFloat(forecastData.day_snow) * 2) + 'px', display: ((parseFloat(forecastData.day_snow) > 0) ? 'block' : 'none') }}></div>
+                <div className='weather-widget-forecast-precipitation'>
+                    <span>{forecastData.day_rain}<br style={{ display: ((parseFloat(forecastData.day_snow) > 0 && parseFloat(forecastData.day_snow) < 18) ? 'block' : 'none') }}></br>{forecastData.day_snow}</span>
+                    <div className='weather-widget-forecast-precipitation-rain' style={{ height: (parseFloat(forecastData.day_rain) * 2) + 'px', display: ((parseFloat(forecastData.day_rain) > 0) ? 'block' : 'none') }}></div>
+                    <div className='weather-widget-forecast-precipitation-snow' style={{ height: (parseFloat(forecastData.day_snow) * 2) + 'px', display: ((parseFloat(forecastData.day_snow) > 0) ? 'block' : 'none') }}></div>
                 </div>
             </div>
         </div>
--- src/components/weather/weather.js
+++ src/components/weather/weather.js
@@ -3,15 +3,19 @@ import "./weather.css";
 import WeatherWidget from '../weather-widget/weather-widget';
 const Weather = () => {
   const locations = [
-    { location: 'Ottawa', lon: -75.6901106, lat: 45.4208777, count: 1 },
-    { location: 'Sudbury', lon: -81.0071182, lat: 46.5025031, count: 2 },
-    { location: 'Marsoui', lon: -66.0685164, lat: 49.2149677, count: 3 },
-    { location: 'Montreal River Harbour', lon: -84.649356, lat: 47.232487, count: 4 }
+    { location: 'Montreal River Harbour', lon: -84.649356, lat: 47.232487, isFirst:true },
+    { location: 'Cochrane', lon: -81.278790, lat: 49.166792},
+    { location: 'Sudbury', lon: -81.0071182, lat: 46.5025031},
+    { location: 'Mattawa', lon: -78.965881, lat: 46.446380},
+    { location: 'Ottawa', lon: -75.6901106, lat: 45.4208777},
+    { location: 'Treeriding Adventure', lon: -71.656786, lat: 47.081166},
+    { location: 'Auberge 31km', lon: -70.589226, lat: 48.669603}, 
+    { location: 'Chic Chocs', lon: -66.038694, lat: 49.064366}
   ];
   return (
     <div className="weather">
       {locations.map(item =>
-        <WeatherWidget isFirstEntry={item.count === 1} location={item.location} lon={item.lon} lat={item.lat} key={item.location}> {item.location} </WeatherWidget>
+        <WeatherWidget isFirstEntry={item.isFirst} location={item.location} lon={item.lon} lat={item.lat} key={item.location}> {item.location} </WeatherWidget>
       )}
     </div>
   )
