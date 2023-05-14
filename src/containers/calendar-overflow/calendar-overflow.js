28eeac4ed73e876d755320ec841cb10329563f17
Salmizar <chris@salmizar.com>
2023-05-14 13:21:25 -0400

    I believe calendar overflow events and viewer are now complete

---
+++ src/containers/calendar-overflow/calendar-overflow.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+import "./calendar-overflow.css";
+import CalendarEvent from '../calendar-event/calendar-event';
+import { Button } from "../../components/button/button.style";
+
+const CalendarOverflow = ({ events, eventDate, user }) => {
+    const [displayOverflowDialog, setDisplayOverflowDialog] = useState(false);
+    const viewEvents = (e) => {
+        e.stopPropagation();
+        setDisplayOverflowDialog(!displayOverflowDialog);
+        //document.body.addEventListener('click', closeOverflowDialog);
+    }
+    const closeOverflowDialog = (e) => {
+        setDisplayOverflowDialog(!displayOverflowDialog);
+        document.body.removeEventListener('click', closeOverflowDialog);
+    }
+    const closeOverflowView = (e) => {
+
+    }
+    return (
+        <div className='calendar-event-overflow'>
+            <div className='calendar-event-overflow-events' onClick={viewEvents}>
+            {events && Object.keys(events.overflow).length} more event{((events && Object.keys(events.overflow).length > 1) ? 's' : '')}
+            </div>
+            <dialog className='calendar-event-overflow-dialog' style={{ display: (displayOverflowDialog) ? 'block' : 'none' }} onClick={(e) => e.stopPropagation()  }>
+                <div className='calendar-event-overflow-dialog-title'>
+                {eventDate.toLocaleDateString("en-us", { day: 'numeric', month: 'long', weekday: 'short' })}
+                </div>
+                <div className='calendar-event-overflow-event-list'>
+                    {Object.values(events).map((event, index) => {
+                        if (event.eventStartingToday || event.eventStartOfNextWeek) {
+                            return <CalendarEvent key={index} eventIndex={index-1} user={user} event={event} overflowView={true}></CalendarEvent>
+                        }
+                    })}
+                    {Object.values(events.overflow).map((event, index) => {
+                        return <CalendarEvent key={index} eventIndex={3 + index} user={user} event={event} overflowView={true}></CalendarEvent>
+                    }
+                    )}
+                </div>
+                <Button theme="black" className="calendar-event-overflow-close-btn" onClick={closeOverflowDialog}>Close</Button>
+          
+            </dialog>
+        </div>
+    )
+}
+
+export default CalendarOverflow
