71f4cd01cb804e7d6ac20b6b881b68e0049c176e
Salmizar <chris@salmizar.com>
2023-06-19 11:54:53 -0400

    LightHouse Optimizations, now 100% on all tests

--- src/containers/calendar-day/calendar-day.js
+++ src/containers/calendar-day/calendar-day.js
@@ -7,31 +7,35 @@ const CalendarDay = ({ day, month, year, isThisToday, isThisMonth, events, avail
         createEvent(year, month, day);
     }
     return (
-        <div className={((isThisMonth) ? "calendar-day-container" : "calendar-day-container-notthismonth")} onClick={ createNewEvent }>
+        <div className={((isThisMonth) ? "calendar-day-container" : "calendar-day-container-notthismonth")} onClick={createNewEvent}>
             <div className={((isThisToday) ? "calendar-date-of-week-today" : "calendar-date-of-week")}>
                 <div>{day}</div>
             </div>
-            <div className='calendar-events'>
-                {Object.values(events).map((event, index) => {
-                    if (index < availableSlots || (index === availableSlots && index === Object.keys(events).length - 1)) {
-                        if (event.eventStartingToday || event.eventStartOfNextWeek) {
-                            return <CalendarEvent
-                                key={event.event.eId}
-                                eventIndex={index}
-                                eventDate={new Date(year, month, day)}
-                                event={event}></CalendarEvent>
+            {
+                (Object.keys(events).length > 0) ?
+                    <div className='calendar-events'>
+                        {Object.values(events).map((event, index) => {
+                            if ((event.eventStartingToday || event.eventStartOfNextWeek) &&
+                                (index < availableSlots || (index === availableSlots && index === Object.keys(events).length - 1))) {
+                                return <CalendarEvent
+                                    key={event.event.eId}
+                                    eventIndex={index}
+                                    eventDate={new Date(year, month, day)}
+                                    event={event}></CalendarEvent>
+                            }
+                            return null;
                         }
-                    }
-                    return null;
-                }
-                )}
-                <CalendarOverflow
-                    eventDate={new Date(year, month, day)}
-                    user={user}
-                    availableSlots={availableSlots}
-                    events={events}>
-                </CalendarOverflow>
-            </div>
+                        )}
+                        <CalendarOverflow
+                            eventDate={new Date(year, month, day)}
+                            user={user}
+                            availableSlots={availableSlots}
+                            events={events}>
+                        </CalendarOverflow>
+                    </div>
+                    :
+                    null
+            }
         </div>
     )
 }
